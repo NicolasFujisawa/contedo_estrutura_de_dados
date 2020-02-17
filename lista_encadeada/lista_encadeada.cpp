c3db329cc52e09ccd9a61e727f921f270cd4f7e0
NicolasFujisawa <nicolau.fernando330@hotmail.com>
2020-02-16 21:04:14 -0300

    Add project files.

---
+++ lista_encadeada/lista_encadeada.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+/*1 - Faça uma função para concatenar duas listas encadeadas
+
+2 - Faça uma função para remover elementos repetidos de uma lista encadeada (apenas uma varredura).
+
+3 - Faça uma função que converta um vetor em lista encadeada (mantenha a ordem)
+
+4 - Faça uma função para intercalar duas listas encadeadas*/
+
+
+int main()
+{
+    //menu
+    int option = -1;
+    int input, output;
+    while (option != 0)
+    {
+        cout << "=-=-=-Menu-=-=-=" << endl;
+        cout << ".1 Inverter um numero inteiro N. Ex: 123 - 321" << endl;
+        cout << ".2 Dado 'n', imprima ate 0 e em seguida volte a n" << endl;
+        cout << ".3 Inverter um vetor de numeros reais com 100 elementos" << endl;
+        cout << ".0 Sair" << endl;
+
+        cin >> option;
+
+        switch (option)
+        {
+        case 1:
+            
+            break;
+        case 2:
+            
+            break;
+        case 3:
+            
+
+            break;
+        default:
+            break;
+        }
+    }
+}
